71fec8cc6a0da8ec301983e9bb6fb73e1f91caa5
phyo18 <C180268@ccg.ac.jp>
2020-02-03 03:19:03 +0000

    20200203

--- ccmc-05/pdo/pages/restaurants.php
+++ ccmc-05/pdo/pages/restaurants.php
@@ -1,3 +1,30 @@
+<?php
+require_once("../database.php");
+$area = -1;
+if (isset($_REQUEST["area"])) {
+    $area = $_REQUEST["area"];
+}
+
+$pdo = connectDatabase();
+$sql = "select * from restaurants where area=?";
+$pstmt = $pdo->prepare($sql);
+$pstmt ->bindValue(1, $area);
+$pstmt->execute();
+$rs = $pstmt->fetchAll();
+$restraunts = [];
+foreach ($rs as $record) {
+    $id = intval($record["id"]);
+    $name = $record["name"];
+    $detail = $record["detail"];
+    $name = $record["name"];
+    $image = $record["image"];
+    $restraunt = new Restaurant($id, $name, $detail, $image, $area);
+    $restaurent[] = $restaurent;
+}
+echo "ここまでは OK!";
+exit(0);
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 	<head>
@@ -14,8 +41,19 @@
 				<th>詳細</th>
 				<th>画像ファイル名</th>
 				<th>地域ID</th>
+				
 			</tr>
-						<tr>
+			<?php foreach ($restaurent as $restaurent) { ?>
+			
+			<tr>
+			    <td><?= $restaurent->getId() ?></td>
+			    <td><?= $restaurent->getName() ?></td>
+			    <td><?= $restaurent->getDetail() ?></td>
+			    <td><?= $restaurent->getImage() ?></td>
+			    <td><?= $restaurent->getArea() ?></td>
+			</tr>
+			<?php } ?>
+			<!--
 				<td>1</td>
 				<td>Wine Bar ENOTECA</td>
 				<td>常時10種類以上の赤・白ワインをご用意しています。
@@ -36,7 +74,8 @@
 				<td>海が見える、海沿いのレストランです。</td>
 				<td>restaurant_6.jpg</td>
 				<td>2</td>
-			</tr>
+			</tr> 
+			-->
 					</table>
 	</body>
 </html>
